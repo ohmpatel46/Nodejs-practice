ba631ca803b5f6133a33b68b247fa5c8dff0120d
ohmpatel46 <ohmpatel46@gmail.com>
2021-04-01 23:58:34 +0530

    Streams,Pipes

---
+++ pipes.js
@@ -0,0 +1,11 @@
+const fs=require('fs');
+const zlib=require('zlib');
+const gzip=zlib.createGzip();
+const gunzip=zlib.createGunzip();
+// const readStream=fs.createReadStream('./example.txt','utf8');
+// const writeStream=fs.createWriteStream('./example2.txt.gz');
+// console.log(gzip);
+// readStream.pipe(gzip).pipe(writeStream);
+const readStream=fs.createReadStream('./example2.txt.gz');
+const writeStream=fs.createWriteStream('./uncompressed.txt');
+readStream.pipe(gunzip).pipe(writeStream);
